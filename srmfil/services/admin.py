ff7f2a90cbfd89bf59e2a278e2b9d72b4f78b13c
seldon1128 <seldon1128@gmail.com>
2026-05-30 17:39:35 -0600

    feat: panel admin Django configurado

--- srmfil/services/admin.py
+++ srmfil/services/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Servicio
 
-# Register your models here.
+@admin.register(Servicio)
+class ServicioAdmin(admin.ModelAdmin):
+
+    list_display = ("id", "nombre", "descripcion")
+
+    search_fields = ("nombre",)
+
+    ordering = ("nombre",)
+
+    list_per_page = 10
