484e686e45eade238b8b3fc7f1f1d2bab74061c0
erick6aelgg <erick6aelgg@ciencias.unam.mx>
2026-06-02 15:33:44 -0600

    fix: Se corrigen los permisos en la creación del super usuario para que actúe como admin.

--- srmfil/users/models.py
+++ srmfil/users/models.py
@@ -1,9 +1,24 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+class UsuarioManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('El email es obligatorio')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('tipo_usuario', 'admin')
+        return self.create_user(email, password, **extra_fields)
 
 class Usuario(AbstractUser):
-    email = models.EmailField(unique=True)
+    objects = UsuarioManager()
 
+    email = models.EmailField(unique=True)
     apellido_p = models.CharField(max_length=100)
     apellido_m = models.CharField(max_length=100)
 
@@ -12,7 +27,6 @@ class Usuario(AbstractUser):
         ('admin', 'Admin'),
     )
     tipo_usuario = models.CharField(max_length=10, choices=TIPO_USUARIO)
-
 
     def __str__(self):
         return self.email
